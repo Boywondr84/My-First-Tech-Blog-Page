caea3e108682abcccd140fa916599a947bb5f237
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-25 23:25:33 -0500

    added css stylings. added homepage routes and homepage handlebars to display first postsgit add -A

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 // const { post } = require('.');
 const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
 
 
 router.get('/', (req, res) => {
-    
-})
+    Post.findAll()
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
