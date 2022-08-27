a43e597e197c8a28e0d6a0beac6b67c3b95756fb
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-26 23:36:39 -0500

    added sign up form and code to login.js file. added user through the webpage and verified new user was added to db which it was.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -21,4 +21,30 @@ async function loginFormHandler(event) {
     }
 }
 
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#signup-username').value.trim();
+    const email = document.querySelector('#signup-email').value.trim();
+    const password = document.querySelector('#signup-password').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
