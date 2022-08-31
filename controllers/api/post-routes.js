130edf16c812f35bd01a932f2244dac6bf7a8abb
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-31 04:27:44 -0500

    added css styling

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -7,6 +7,22 @@ router.get('/', (req, res) => {
         attributes: [
             'id', 'title', 'created_at'
         ],
+        order: [['created_at', 'DESC']],
+
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -16,8 +32,33 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    Post.findOne()
-        .then(dbPostData => res.json(dbPostData))
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No Post found with that ID" });
+                return;
+            }
+            res.json(dbPostData);
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
