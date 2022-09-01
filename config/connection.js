f71ff0457905caf6335f8e4b84b9560f87b5bf7c
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-31 20:51:50 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,20 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // // need jawsdb_url here (heroku)
+let sequelize;
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize - new Sequelize(process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
     {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    // logging: false
-});
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+        // logging: false
+    });
+}
 
 module.exports = sequelize;
